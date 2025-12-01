6ce726e61703dcf9989dfb2dcbbdf5a6d767d067
Matimateokol <mateuszkolacz2019@gmail.com>
2025-12-02 00:10:27 +0100

    Remove read-the-docs paragraph

--- frontend-do-you-know-da-wae/src/App.jsx
+++ frontend-do-you-know-da-wae/src/App.jsx
@@ -28,9 +28,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
       <Map
         initialViewState={{
           longitude: -100,
